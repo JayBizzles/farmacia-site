0d857951561037dc508333b16295cf44717729f0
JayBizzles <jbills@ucmerced.edu>
2020-10-06 22:02:40 -0700

    Added the navbar to the app-component.html for easier routing and simplicity

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
+
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { AcercaComponent } from './acerca/acerca.component';
+import { ContactenosComponent } from './contactenos/contactenos.component';
+import { FajasYDietasComponent } from './fajas-y-dietas/fajas-y-dietas.component';
+import { HierbasComponent } from './hierbas/hierbas.component';
+import { MedicinasComponent } from './medicinas/medicinas.component';
+import { MiscelanosComponent } from './miscelanos/miscelanos.component';
+import { PerfumesComponent } from './perfumes/perfumes.component';
+import { PerfumesCaballerosComponent } from './perfumes-caballeros/perfumes-caballeros.component';
+import { PerfumesDamasComponent } from './perfumes-damas/perfumes-damas.component';
+import { PharmaciaComponent } from './pharmacia/pharmacia.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'acerca', component: AcercaComponent },
+  { path: 'contactenos', component: ContactenosComponent },
+  { path: 'fajas-y-dietas', component: FajasYDietasComponent },
+  { path: 'hierbas', component: HierbasComponent },
+  { path: 'medicinas', component: MedicinasComponent },
+  { path: 'miscelanos', component: MiscelanosComponent },
+  { path: 'perfumes', component: PerfumesComponent },
+  { path: 'perfumes-caballeros', component: PerfumesCaballerosComponent },
+  { path: 'perfumes-damas', component: PerfumesDamasComponent },
+  { path: 'pharmacia', component: PharmaciaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
